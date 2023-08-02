28a171d57681edae01428febb8d0a43d039772f9
신현진 <blackduvet52@gmail.com>
2023-08-02 10:06:37 +0900

    [현진]폴더/파일취합 0802
    1. 로그인 기능 페이지
    2. 서버연결 폴더
    3. 모듈 설치

---
+++ config/datebase.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+
+let conn = {
+  host: "localhost",
+  user: "root",
+  password: "coavldjs3372!@",
+  port: "3306",
+  database: "nodejs_DB",
+};
+
+module.exports = {
+  init: () => {
+    return mysql.createConnection(conn);
+  },
+};
