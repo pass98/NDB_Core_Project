731ce6cd769ae7911655e5d3ecf56f35b5489ece
나범수 <123361606+pass98@users.noreply.github.com>
2023-08-12 13:27:45 +0900

    메인페이지 - 마이페이지 연동, 구독 모델 선택 후 마이페이지로 들어가는 기능 구현

--- router/mypage.js
+++ router/mypage.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const db = require("../config/datebase");
+const mp_subs_img = {
+    'basic' : 'public/img/베이직.png',
+    'standard' : 'public/img/스탠다드.png',
+    'pro' : 'public/img/BASIC.jpg'
+};
 
 
 // 사용자 프로필 페이지
@@ -9,4 +15,6 @@ router.get("/mypage", (req, res) => {
       res.render("myPage.html");
   });
 
+ 
+  
   module.exports = router;
