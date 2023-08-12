fedbebb71628a9c20219d2c65e5d17d38dd1c552
나범수 <123361606+pass98@users.noreply.github.com>
2023-08-13 00:55:05 +0900

    [범수_main]마이페이지 복구화 완성(만세!)
    1. subs_modal의 css와 분리 성공
    2. 구독창 폰트 정상화
    3. 이름창, 닉네임창 데이터베이스 연동
    4. 남은일 : 로그인과 세션 연동, 텝매뉴 각 정보 연동

--- router/mypage.js
+++ router/mypage.js
@@ -10,11 +10,22 @@ const mp_subs_img = {
 
 
 // 사용자 프로필 페이지
+let conn = db.init()
 router.get("/mypage", (req, res) => {
-      // res.send(`<h1>Hello, ${req.user.displayName}</h1>`);
-      res.render("myPage.html");
-  });
-
- 
-  
+    conn.connect()
+    const sql =  "SELECT USER_ID, MEMBER_NAME FROM MEMBER LIMIT 1";
+    conn.query(sql, (err, results) => {
+        if(!err){
+            const data = results[0]
+            console.log('사이트 접속 성공!')
+            
+            res.render("myPage.html", {id: data.USER_ID, name: data.MEMBER_NAME});
+            console.log(data)
+        }else{
+            console.log('사이트 접속 실패!')
+        }
+       
+    });
+    
+});
   module.exports = router;
