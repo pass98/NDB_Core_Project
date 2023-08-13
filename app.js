15a1cea8ff861cc0be33ed3fa5cd0e821985323c
나범수 <123361606+pass98@users.noreply.github.com>
2023-08-13 15:36:49 +0900

    [나범수_main]리뷰노트 추가 중
    1.reviewNote page 생성 중
    2.현재 css 및 데이터 연동 필요

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const passport = require("passport");
 const path = require("path");
 const app = express();
 
+
+app.get('/mypage/reVeiw', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'reviewNote.html'));
+});
+
+
 // Nunjucks 설정
 app.set("view engine", "html");
 nunjucks.configure("views", { express: app, watch: true });
@@ -33,6 +39,8 @@ const joinRouter = require("./router/login");
 app.use(joinRouter);
 const indexRouter = require("./router/codeExam");
 app.use(indexRouter);
+const mypageRouter = require("./router/mypage");
+app.use(mypageRouter);
 
 app.listen(3003, () => {
   console.log("서버가 3003번 포트에서 실행 중입니다.");
